2b954d97b54612f649c6e7d6b7ffade15a23685d
Arthur Ribeiro de Cerqueira <arthur.cerqueira@fbter.org.br>
2026-05-19 10:15:50 -0300

    Codigos exemplo de teste para o projeto

--- Game2D_Alien/tests/test_desconto.py
+++ Game2D_Alien/tests/test_desconto.py
@@ -1,5 +1,17 @@
-from src.desconto import DescontoNormal, DescontoVIP
+from src.desconto import DescontoNormal, DescontoVIP, DescontoPremium
 import pytest
+
+@pytest.mark.parametrize("valor, esperado", [
+    (100, 30),
+    (200, 60),
+    (300, 90)
+])
+
+def test_desconto_premium(valor, esperado):
+    desconto = DescontoPremium()
+    resultado = desconto.calcular(valor)
+    
+    assert resultado == esperado
 
 def test_desconto_normal():
     desconto = DescontoNormal()
